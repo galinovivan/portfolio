6cc07429ce8d932ea235f56fba358009aea8f6d1
ivan galinov <ivan galinov>
2016-10-01 07:14:07 +0300

    start new template

--- js/preload.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function() {
-   var preloader = $('.page_preloader');
-    var spinner = preloader.find('.spinner');
-    spinner.fadeOut();
-    preloader.delay(350).fadeOut('slow');
-});
