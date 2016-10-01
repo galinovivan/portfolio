c5711c4da5c4355c1c8f58c6411a4f5bdc285d14
galinovivan <9225913@gmail.com>
2016-10-02 00:49:38 +0300

    nav

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,11 @@
 <div class="sidebar animate_swim_right">
-
+<nav class="side_nav">
+    <ul class="list_inline">
+        <li><a href="index.php">Главная</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+</nav>
 </div>
