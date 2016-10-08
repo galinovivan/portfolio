3c5145ba83107ec8f4c9a370769a10e29f61acc9
ivan galinov <ivan galinov>
2016-10-08 23:02:08 +0300

    chang

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
 <title>Главная</title>
 
 
-<link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
 
 
 <link rel="stylesheet" href="css/style.css">
 
 
 <script src="libs/jquery/jquery-3.1.1.min.js"></script>
+<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="https://use.fontawesome.com/bf8c20532a.js"></script>
 
 
 </head>
@@ -26,10 +29,35 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-12 col-md-6 main_carousel main_left_column full_screen_column">
-			<div class="">
-					<img src="img/site_1.png" alt="site" class="img_responsive">
+				<div class="carousel slide" data-interval="2000" data-ride="carousel" id="siteCarousel">
+					<div class="carousel-inner">
+					<div class="item active hide_text_container">
+						<div class="shadow_block"></div>
+						<div class="carousel-caption">
+								<span class="hide_text">
+								Бихевиоризм обнажен. Несомненный интерес представляет
+								и тот факт, что океаническое ложе сохраняет соленосный артезианский бассейн.
+								Имея такие данные, можно сделать существенный вывод о том,
+								что субъект власти отражает конструктивный постиндустриализм.</span>
+						</div>
+			<div class="img_wrapper">
+					<img src="img/site_1.png" alt="site">
+				</div>
+			</div>
+			<div class="item">
+				<div class="img_wrapper">
+					<img src="img/exp.jpg" alt="site">
+				</div>
+				</div>
+				<a class="carousel-control left" data-slide="prev" href="#siteCarousel">
+					<i class="fa fa-angle-left"></i>
+				</a>
+					<a class="carousel-control right" data-slide="next" href="#siteCarousel">
+					<i class="fa fa-angle-right"></i>
+					</a>
 			</div>
 			</div>
+		</div>
 			<div class="col-xs-12 col-md-6 full_screen_column">
 				<div class="row">
 					<div class="col-xs-12 col-md-12 main_right_column">
@@ -47,7 +75,7 @@
 		</div>
 	</div>
 </main>
-<script src="js/carousel.js"></script>
+
 <script type="text/javascript" src="js/preloader.js"></script>
 </body>
 </html>
